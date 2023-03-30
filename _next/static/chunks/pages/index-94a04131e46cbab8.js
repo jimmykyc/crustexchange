6318259e19667cf0317e3b53f70e2df8be7a6331
Jimmy Dev <81708489+jimmykyc@users.noreply.github.com>
2023-03-30 08:19:07 +0800

    Add files via upload

---
+++ _next/static/chunks/pages/index-94a04131e46cbab8.js
@@ -0,0 +1,208 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [405], {
+        48312: function (s, e, t) {
+            (window.__NEXT_P = window.__NEXT_P || []).push(["/", function () {
+                return t(5075)
+            }])
+        },
+        5075: function (s, e, t) {
+            "use strict";
+            t.r(e), t.d(e, {
+                default: function () {
+                    return d
+                }
+            });
+            var a = t(85893),
+                l = t(45106),
+                i = t(57741),
+                c = t(26166),
+                n = t(86e3),
+                r = t(28822),
+                x = t(40570),
+                m = t(41664),
+                o = t.n(m);
+
+            function d() {
+                return (0, a.jsxs)(n.Z, {
+                    title: "Crust Exchange",
+                    children: [(0, a.jsxs)("section", {
+                        className: "min-h-screen py-6 px-6 lg:px-36 lg:py-12 relative contentMasthead",
+                        children: [(0, a.jsx)(c.Z, {}), (0, a.jsxs)("div", {
+                            className: "w-full xl:w-2/5 lg:w-3/5 mt-36 lg:mt-48",
+                            children: [(0, a.jsxs)("div", {
+                                className: "text-5xl lg:text-7xl font-bold",
+                                children: ["The ", (0, a.jsx)("span", {
+                                    className: "gradientText",
+                                    children: "Core Value"
+                                }), " of Perpetual DEXs"]
+                            }), (0, a.jsx)(l.Z, {}), (0, a.jsx)("div", {
+                                className: "mt-12 text-grey",
+                                children: "Get Crust Exchange info before anyone else by subscribing to our exclusive newsletter."
+                            })]
+                        }), (0, a.jsx)("div", {
+                            className: "mt-24 lg:mt-0 lg:absolute flex justify-center lg:block lg:px-0 left-0 right-0 bottom-24 lg:right-36",
+                            style: {
+                                marginRight: "-.5rem"
+                            },
+                            children: (0, a.jsx)(r.Z, {})
+                        })]
+                    }), (0, a.jsx)("section", {
+                        className: "min-h-screen py-6 px-6 md:px-48 md:py-36 relative contentWaveform",
+                        children: (0, a.jsxs)("div", {
+                            className: "w-4/5 mx-auto lg:w-full flex flex-wrap",
+                            children: [(0, a.jsxs)("div", {
+                                className: "w-full lg:w-1/2 xl:px-24 flex flex-col",
+                                children: [(0, a.jsxs)("div", {
+                                    className: "flex items-center mb-24 timeline",
+                                    children: [(0, a.jsx)("img", {
+                                        className: "w-24 mr-6",
+                                        src: "./assets/icon-beta.png"
+                                    }), (0, a.jsxs)("div", {
+                                        children: [(0, a.jsx)("div", {
+                                            className: "text-2xl font-bold",
+                                            children: "Testnet Opens"
+                                        }), (0, a.jsx)("div", {
+                                            className: "text-xl text-grey mt-3",
+                                            children: "March 16th, 2023"
+                                        })]
+                                    })]
+                                }), (0, a.jsxs)("div", {
+                                    className: "flex items-center mb-24 timeline",
+                                    children: [(0, a.jsx)("img", {
+                                        className: "w-24 mr-6",
+                                        src: "./assets/icon-launch.png"
+                                    }), (0, a.jsxs)("div", {
+                                        children: [(0, a.jsx)("div", {
+                                            className: "text-2xl font-bold",
+                                            children: "Official Launch"
+                                        }), (0, a.jsx)("div", {
+                                            className: "text-xl text-grey mt-3",
+                                            children: "Soon"
+                                        })]
+                                    })]
+                                }), (0, a.jsxs)("div", {
+                                    className: "flex items-center",
+                                    children: [(0, a.jsx)("img", {
+                                        className: "w-24 mr-6",
+                                        src: "./assets/icon-fiat-ramp.png"
+                                    }), (0, a.jsx)("div", {
+                                        children: (0, a.jsx)("div", {
+                                            className: "text-2xl font-bold",
+                                            children: "Infinite Potential"
+                                        })
+                                    })]
+                                })]
+                            }), (0, a.jsxs)("div", {
+                                className: "z-50 w-full lg:w-1/2 xl:px-24 mt-24 lg:mt-0 lg:pl-24 lg:text-left text-center",
+                                children: [(0, a.jsx)("div", {
+                                    className: "text-4xl font-bold mb-12",
+                                    children: "Beta Is Live!"
+                                }), (0, a.jsxs)("div", {
+                                    className: "text-lg text-grey mb-12",
+                                    children: ["Our beta program is officially live on Core. It includes access to trading, staking, and a range of exclusive beta incentives that are available throughout the beta testing period.",
+                                     (0, a.jsx)("br", {}), (0, a.jsx)("br", {}), "Don't miss out on the limited-time opportunity! Connect your wallet now to gain access to the Crust Exchange platform.",
+                                     (0, a.jsx)("br", {}), (0, a.jsx)("br", {}), "Crust Contract: 0x29665752a02067DdaCaA4E76faF7f8871823da1c",
+                                     (0, a.jsx)("br", {}), (0, a.jsx)("br", {}), "Crust Email Address: admin@crustexchange.com"
+                                    ]
+                                }), (0, a.jsx)(x.Z, {
+                                    color: "glass-teal",
+                                    size: "lg",
+                                    children: (0, a.jsx)(o(), {
+                                        className: "block rounded-lg gradientBg px-6 py-3 w-36 text-center text-white mx-auto lg:mx-0",
+                                        href: "",
+                                        children: "Launch Mainnet"
+                                    })
+                                })]
+                            })]
+                        })
+                    }), (0, a.jsxs)("section", {
+                        className: "py-6 px-6 md:px-48 md:py-36",
+                        children: [(0, a.jsx)("div", {
+                            className: "text-3xl font-bold text-center mb-12",
+                            children: "Pushing the DEX Frontier"
+                        }), (0, a.jsxs)("div", {
+                            className: "text-grey text-lg w-full lg:w-3/5 block mx-auto text-center",
+                            children: [(0, a.jsx)("p", {
+                                className: "mb-6",
+                                children: "The Core Value, starting with perpetual trading."
+                            }), (0, a.jsx)("p", {
+                                className: "mb-6",
+                                children: "Our architecture was tailored to push the limits of decentralized trading, with a technical stack built to support advanced charting functionality, new synthetic assets, trading automation, and much more."
+                            })]
+                        }), (0, a.jsxs)("div", {
+                            className: "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 mt-24",
+                            children: [(0, a.jsxs)("div", {
+                                className: "mb-24",
+                                children: [(0, a.jsx)("img", {
+                                    className: "block mx-auto mb-6 w-24",
+                                    src: "./assets/icon-advanced-automation.png"
+                                }), (0, a.jsx)("div", {
+                                    className: "text-center",
+                                    children: "Advanced automation"
+                                })]
+                            }), (0, a.jsxs)("div", {
+                                className: "mb-24",
+                                children: [(0, a.jsx)("img", {
+                                    className: "block mx-auto mb-6 w-24",
+                                    src: "./assets/icon-leverage.png"
+                                }), (0, a.jsx)("div", {
+                                    className: "text-center",
+                                    children: "Up to 100x leverage"
+                                })]
+                            }), (0, a.jsxs)("div", {
+                                className: "mb-24",
+                                children: [(0, a.jsx)("img", {
+                                    className: "block mx-auto mb-6 w-24",
+                                    src: "./assets/icon-flexible-assets.png"
+                                }), (0, a.jsx)("div", {
+                                    className: "text-center",
+                                    children: "Flexible assets"
+                                })]
+                            }), (0, a.jsxs)("div", {
+                                className: "mb-24",
+                                children: [(0, a.jsx)("img", {
+                                    className: "block mx-auto mb-6 w-24",
+                                    src: "./assets/icon-fiat-ramp.png"
+                                }), (0, a.jsx)("div", {
+                                    className: "text-center",
+                                    children: "Fiat on/offboarding"
+                                })]
+                            }), (0, a.jsxs)("div", {
+                                className: "mb-24",
+                                children: [(0, a.jsx)("img", {
+                                    className: "block mx-auto mb-6 w-24",
+                                    src: "./assets/icon-multilingual.png"
+                                }), (0, a.jsx)("div", {
+                                    className: "text-center",
+                                    children: "Multi-lingual support"
+                                })]
+                            }), (0, a.jsxs)("div", {
+                                className: "mb-24",
+                                children: [(0, a.jsx)("img", {
+                                    className: "block mx-auto mb-6 w-24",
+                                    src: "./assets/icon-notifications.png"
+                                }), (0, a.jsx)("div", {
+                                    className: "text-center",
+                                    children: "Real-time notifications"
+                                })]
+                            })]
+                        }), (0, a.jsx)("img", {
+                            className: "w-4/5 lg:w-2/5 block mx-auto mt-12 lg:mt-36",
+                            src: "./assets/dna.png"
+                        })]
+                    }), (0, a.jsx)(i.Z, {
+                        cta: !0
+                    })]
+                })
+            }
+        }
+    },
+    function (s) {
+        s.O(0, [503, 102, 972, 724, 834, 885, 159, 193, 774, 888, 179], (function () {
+            return e = 48312, s(s.s = e);
+            var e
+        }));
+        var e = s.O();
+        _N_E = e
+    }
+]);
